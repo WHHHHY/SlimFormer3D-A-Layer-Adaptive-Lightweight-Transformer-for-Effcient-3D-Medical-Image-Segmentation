472be391ce8a55550e8e7256eb9fef7556487fc2
WHHHHY <yang_hong0822@163.com>
2025-12-11 18:27:25 +0800

    first commit

--- lib/metrics/Abdomen/DICE.py
+++ lib/metrics/Abdomen/DICE.py
@@ -1,11 +1,3 @@
-# -*- encoding: utf-8 -*-
-"""
-@author   :   yykzjh    
-@Contact  :   yykzhjh@163.com
-@DateTime :   2023/5/1 18:36
-@Version  :   1.0
-@License  :   (C)Copyright 2023
-"""
 import torch
 import torch.nn as nn
 
@@ -14,18 +6,9 @@ from lib.utils import *
 
 class DICE(object):
     def __init__(self, num_classes=2, sigmoid_normalization=False, mode="extension"):
-        """
-        定义DICE系数评价指标计算器
-        Args:
-            num_classes: 类别数
-            sigmoid_normalization: 对网络输出采用sigmoid归一化方法，否则采用softmax
-            mode: DSC的计算方式，"standard":标准计算方式；"extension":扩展计算方式
-        """
         super(DICE, self).__init__()
-        # 初始化参数
         self.num_classes = num_classes
         self.mode = mode
-        # 初始化sigmoid或者softmax归一化方法
         if sigmoid_normalization:
             self.normalization = nn.Sigmoid()
         else:
@@ -33,23 +16,13 @@ class DICE(object):
 
 
     def __call__(self, input, target):
-        """
-        计算DICE系数
-        Args:
-            input: 网络模型输出的预测图,(B, C, H, W)
-            target: 标注图像,(B, H, W)
 
-        Returns:
-        """
-        # ont-hot处理，将标注图在axis=1维度上扩张，该维度大小等于预测图的通道C大小，维度上每一个索引依次对应一个类别,(B, C, H, W)
         target = expand_as_one_hot(target.long(), self.num_classes)
 
-        # 判断one-hot处理后标注图和预测图的维度是否都是4维
-        assert input.dim() == target.dim() == 5, "one-hot处理后标注图和预测图的维度不是都为4维！"
-        # 判断one-hot处理后标注图和预测图的尺寸是否一致
-        assert input.size() == target.size(), "one-hot处理后预测图和标注图的尺寸不一致！"
+        assert input.dim() == target.dim() == 5, "After one-hot encoding, both prediction and label tensors are expected to be 5-dimensional."
 
-        # 对预测图进行Sigmiod或者Sofmax归一化操作
+        assert input.size() == target.size(), "After one-hot encoding, the prediction and label tensors must match in shape."
+
         input = self.normalization(input)
 
         return compute_per_channel_dice(input, target, epsilon=1e-6, mode=self.mode)
--- lib/metrics/Amos/IoU.py
+++ lib/metrics/Amos/IoU.py
@@ -1,34 +1,18 @@
-# -*- encoding: utf-8 -*-
-"""
-@author   :   yykzjh    
-@Contact  :   yykzhjh@163.com
-@DateTime :   2023/6/13 2:54
-@Version  :   1.0
-@License  :   (C)Copyright 2023
-"""
 import math
 import torch
 import torch.nn as nn
 import numpy as np
 
 import sys
-sys.path.append(r"D:\Projects\Python\3D-tooth-segmentation\PMFS-Net：Polarized Multi-scale Feature Self-attention Network For CBCT Tooth Segmentation\my-code")
 from lib.utils import *
 
 
 
 class IoU(object):
     def __init__(self, num_classes=33, sigmoid_normalization=False):
-        """
-        定义IoU评价指标计算器
 
-        :param num_classes: 类别数
-        :param sigmoid_normalization: 对网络输出采用sigmoid归一化方法，否则采用softmax
-        """
         super(IoU, self).__init__()
-        # 初始化参数
         self.num_classes = num_classes
-        # 初始化sigmoid或者softmax归一化方法
         if sigmoid_normalization:
             self.normalization = nn.Sigmoid()
         else:
@@ -36,21 +20,11 @@ class IoU(object):
 
 
     def __call__(self, input, target):
-        """
-        IoU
 
-        :param input: 网络模型输出的预测图,(B, C, H, W)
-        :param target: 标注图像,(B, H, W)
-        :return:
-        """
-        # 对预测图进行Sigmiod或者Sofmax归一化操作
         input = self.normalization(input)
 
-        # 将预测图像进行分割
         seg = torch.argmax(input, dim=1)
-        # 判断预测图和真是标签图的维度大小是否一致
-        assert seg.shape == target.shape, "seg和target的维度大小不一致"
-        # 转换seg和target数据类型为整型
+        assert seg.shape == target.shape, "dimensions of seg and target do not match"
         seg = seg.type(torch.uint8)
         target = target.type(torch.uint8)
 
